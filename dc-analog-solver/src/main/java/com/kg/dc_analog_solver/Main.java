aa4872cff72acc61838626c2e41b4affe14d63f3
Khushi Goel <khushigoel927@berkeley.edu>
2025-11-24 20:01:09 -0500

    Created Basic Project Directory Structure

---
+++ dc-analog-solver/src/main/java/com/kg/dc_analog_solver/Main.java
@@ -0,0 +1,10 @@
+package com.kg.dc_analog_solver;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
